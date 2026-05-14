08962b7e37beb074689b55c4f1db42c42ed72e05
Harsh-Prajapati2 <harshprajapati020105@gmail.com>
2026-05-14 19:55:06 +0530

    solve build error

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext',
-    minify: 'esbuild',
     cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('gsap')) return 'gsap-vendor'
-            if (id.includes('three')) return 'three-vendor'
-            if (id.includes('lenis')) return 'lenis-vendor'
-            if (id.includes('react-router')) return 'router-vendor'
-            if (id.includes('react-dom')) return 'react-dom-vendor'
-            if (id.includes('react')) return 'react-vendor'
-          }
-        },
-      },
-    },
   },
 })
